c68a50fe8e050526aac1c9ec93e48cb1b34d335b
David Park <bornskilled200@gmail.com>
2015-04-16 14:55:41 -0400

    download different package that has autoconf and use sudo to install

--- travis/install-libuv.sh
+++ travis/install-libuv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/libuv/libuv/archive/v0.10.36.tar.gz
-tar -xzvf v0.10.36.tar.gz
-cd v0.10.36 && ./configure --prefix=/usr && make && sudo make install
+wget https://github.com/libuv/libuv/archive/v1.4.2.tar.gz
+tar -xzvf v1.4.2.tar.gz
+cd libuv-1.4.2 && ./autogen.sh && ./configure --prefix=/usr && make && sudo make install
